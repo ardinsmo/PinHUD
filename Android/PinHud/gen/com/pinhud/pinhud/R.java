fea76b99f5190c70c66bfb2bd73d399a6ac1daad
Austin Browne <dazztrazak@gmail.com>
2014-01-17 23:55:00 -0500

    Start of FeedFragment

--- Android/PinHud/gen/com/pinhud/pinhud/R.java
+++ Android/PinHud/gen/com/pinhud/pinhud/R.java
@@ -8,6 +8,10 @@
 package com.pinhud.pinhud;
 
 public final class R {
+    public static final class anim {
+        public static final int slide_left=0x7f040000;
+        public static final int slide_right=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,8 +21,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int custombutton=0x7f020000;
@@ -26,31 +30,37 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
         public static final int login_button_custom=0x7f020003;
         public static final int pressedbutton=0x7f020004;
-        public static final int target_list_logo=0x7f020005;
+        public static final int search_bg_shadow=0x7f020005;
+        public static final int target_list_logo=0x7f020006;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int action_settings=0x7f080005;
-        public static final int email=0x7f080002;
-        public static final int imageView1=0x7f080004;
-        public static final int login=0x7f080003;
-        public static final int password=0x7f080001;
-        public static final int progress=0x7f080000;
-=======
-        public static final int action_settings=0x7f090000;
->>>>>>> 83109a2f5cc26d4a1ab41ab478ec10d81916fb97
+        public static final int action_settings=0x7f0b0009;
+        public static final int card_rel=0x7f0b0008;
+        public static final int email=0x7f0b0005;
+        public static final int imageView1=0x7f0b0007;
+        public static final int login=0x7f0b0006;
+        public static final int newList=0x7f0b0002;
+        public static final int password=0x7f0b0004;
+        public static final int progress=0x7f0b0003;
+        public static final int scrollView=0x7f0b0000;
+        public static final int theLayout=0x7f0b0001;
+    }
+    public static final class integer {
+        public static final int config_slide_time=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int login_layout=0x7f030001;
+        public static final int feed_layout=0x7f030001;
+        public static final int login_layout=0x7f030002;
+        public static final int main_list_card=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f0a0000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int action_settings=0x7f080001;
+        public static final int app_name=0x7f080000;
+        public static final int hello_world=0x7f080002;
     }
     public static final class style {
         /** 
@@ -74,13 +84,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f090001;
+        public static final int nowCardStyle=0x7f090002;
     }
     public static final class xml {
-        public static final int nfc_tech_filter=0x7f040000;
+        public static final int nfc_tech_filter=0x7f050000;
     }
 }
--- Android/PinHud/src/com/pinhud/pinhud/FeedFragment.java
+++ Android/PinHud/src/com/pinhud/pinhud/FeedFragment.java
@@ -1,7 +1,35 @@
 package com.pinhud.pinhud;
 
+import java.util.Locale;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class FeedFragment extends Fragment {
 
+		private CardLayout layout;
+		private RelativeLayout newCard;
+		
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) {
+	        // Inflate the layout for this fragment
+	        View view = inflater.inflate(R.layout.feed_layout, container,
+	                false);
+	        
+	        layout = (CardLayout) view.findViewById(R.id.theLayout);
+	        newCard = (RelativeLayout) view.findViewById(R.id.card_rel);
+	        newCard = (RelativeLayout) View.inflate(this.getActivity(), R.layout.main_list_card, null);
+	        layout.addView(newCard);
+			newCard.startAnimation(AnimationUtils.loadAnimation(newCard.getContext(),
+					R.anim.slide_right));
+			
+	        return view;
+	    }
 }
