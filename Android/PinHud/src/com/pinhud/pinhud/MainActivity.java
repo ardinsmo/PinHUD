83109a2f5cc26d4a1ab41ab478ec10d81916fb97
brmahar <brmahar@mtu.edu>
2014-01-17 22:36:52 -0500

    Added some NFC error checking

--- Android/PinHud/src/com/pinhud/pinhud/MainActivity.java
+++ Android/PinHud/src/com/pinhud/pinhud/MainActivity.java
@@ -1,22 +1,53 @@
 package com.pinhud.pinhud;
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private NfcAdapter mNfcAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		if(mNfcAdapter == null){
+			Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
+		if (!mNfcAdapter.isEnabled()){
+			new AlertDialog.Builder(this).setTitle("NFC Disabled")
+			.setMessage("Please enable your NFC to use this app.")
+			.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface arg0, int arg1) {
+					startActivity(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS));
+				}
+			})
+			.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) { 
+					// do nothing
+				}
+			})
+			.show();
+
+		}else{
+			handleFragments(getIntent());
+		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void handleFragments(Intent intent) {
+
+
 	}
 
 }
