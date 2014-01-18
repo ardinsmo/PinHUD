36c947564f634fb152a7d66a6c66942995655f47
Alex Dinsmoor <ardinsmo@mtu.edu>
2014-01-18 17:17:06 -0500

    Suckin on the test of MHacks

--- Website/dist/js/launch2.js
+++ Website/dist/js/launch2.js
@@ -3,11 +3,14 @@ google.load("visualization", "1", {packages:["corechart"]});
 var theResult;
 var TestObject
 var username;
+alert("butts");
 function parse(){
+    alert("#butts");
     Parse.initialize("2ICGEDAbu9xpPISmefOo6y4VvEYcw7W0oOF7Lp2T", "6tOKR4v4ERtZhk8VHahuCjoinJzZkTOcUxRPWKc5");
+    alert("woring");
     TestObject = Parse.Object.extend("Pins");
     username=Parse.User.current().get("username");
-    document.getElementById("welcome").innerHTML="Welcome, "+username+"!";
+    //document.getElementById("welcome").innerHTML="Welcome, "+username+"!";
     /*var testObject = new TestObject();
     testObject.set("userId","Alex");
     testObject.save(null, {
@@ -24,12 +27,13 @@ function parse(){
     var query = new Parse.Query(TestObject);
 query.equalTo("userId", username);
 query.descending("Repins");
-query.limit(250);
+alert("yoyoyo");
+query.limit(10);
 query.find({
   success: function(results) {
      theResult=results;
      document.getElementById("pin_count").innerHTML="You currently have "+results.length+" pins in our database!";
-    //alert("Successfully retrieved " + results.length + " scores.");
+    alert("Successfully retrieved " + results.length + " scores.");
     // Do something with the returned Parse.Object values
     
     for (var i = 0; i < results.length; i++) { 
@@ -136,77 +140,6 @@ function insertTable()
 }
 
 
-function toggleForce(id){
-    //alert(id);
-    
-    var query = new Parse.Query(TestObject);
-query.equalTo("userId", username);
-query.equalTo("PinID",id);
-
-//query.descending("Repins");
-query.find({
-  success: function(results) {
-    //alert(results.length);
-    
-    //alert("Results: "+results[0].get('forceDisplay'));
-    results[0].set("forceDisplay",!results[0].get("forceDisplay"));
-    results[0].save();
-  },
-  error: function(error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
-  
-});
-    
-}
-
-function notify(){
-    //code
-    alert("yolo");
-}
-
-// Builds the HTML Table out of myList.
-function buildHtmlTable() {
-    //parse();
-alert("Passed test");
-alert("The result is "+theResult);
-    var columns = addAllColumnHeaders(theResult);
-alert("Columns is "+columns);
-
-    for (var i = 0 ; i < theResult.length ; i++) {
-        var row$ = $('<tr/>');
-        for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
-            var cellValue = myList[i][columns[colIndex]];
-
-            if (cellValue == null) { cellValue = ""; }
-
-            row$.append($('<td/>').html(cellValue));
-        }
-        $("#excelDataTable").append(row$);
-    }
-}
-
-// Adds a header row to the table and returns the set of columns.
-// Need to do union of keys from all records as some records may not contain
-// all records
-function addAllColumnHeaders(myList)
-{
-    var columnSet = [];
-    var headerTr$ = $('<tr/>');
-
-    for (var i = 0 ; i < myList.length ; i++) {
-        var rowHash = myList[i];
-        for (var key in rowHash) {
-            if ($.inArray(key, columnSet) == -1){
-                columnSet.push(key);
-                headerTr$.append($('<th/>').html(key));
-            }
-        }
-    }
-    $("#excelDataTable").append(headerTr$);
-
-    return columnSet;
-}
 
 
 
