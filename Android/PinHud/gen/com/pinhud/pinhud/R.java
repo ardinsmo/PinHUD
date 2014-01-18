85caa50d8a5246865ee867fc74ef98f4d6b53bb0
Austin Browne <dazztrazak@gmail.com>
2014-01-17 22:50:58 -0500

    LoginFragment progress

--- Android/PinHud/gen/com/pinhud/pinhud/R.java
+++ Android/PinHud/gen/com/pinhud/pinhud/R.java
@@ -21,13 +21,24 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int custombutton=0x7f020000;
+        public static final int edit_back=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int login_button_custom=0x7f020003;
+        public static final int pressedbutton=0x7f020004;
+        public static final int target_list_logo=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int email=0x7f080002;
+        public static final int imageView1=0x7f080004;
+        public static final int login=0x7f080003;
+        public static final int password=0x7f080001;
+        public static final int progress=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int login_layout=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- Android/PinHud/src/com/pinhud/pinhud/LoginFragment.java
+++ Android/PinHud/src/com/pinhud/pinhud/LoginFragment.java
@@ -1,7 +1,71 @@
 package com.pinhud.pinhud;
 
+import java.util.Locale;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginFragment extends Fragment {
+	
+	private Button login;
+	private String getUser;
+	private String getName;
+	private EditText username;
+	private EditText name;
+	private LoginFragment thisThing = this;
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.login_layout, container,
+                false);
+        login = (Button) view.findViewById(R.id.login);
+        
+        login.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				getUser = username.getText().toString().toLowerCase(Locale.US);
+				getName = name.getText().toString();
+				savePreferences("stored", true);
+				savePreferences("User", getUser);
+				savePreferences("Name", getName);
+				Fragment fragment = new FeedFragment();
+			    FragmentTransaction transaction = thisThing.getFragmentManager().beginTransaction();
+			    //transaction.add(R.id.container, fragment, "second");
+			    transaction.addToBackStack(null);
+			    transaction.commit();
+			}
+
+		});
+        
+        return view;
+    }
+	
+	private void savePreferences(String key, String value){
+		SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+
+		Editor edit = shared.edit();
+		edit.putString(key, value);
+		edit.commit();
+	}
+
+	private void savePreferences(String key, boolean value){
+		SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+
+		Editor edit = shared.edit();
+		edit.putBoolean(key, value);
+		edit.commit();
+	}
 
 }
